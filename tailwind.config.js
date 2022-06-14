2d73461fe00e788cc05fbe0c8d686101bbd5491c
revett <2796074+revett@users.noreply.github.com>
2022-06-14 22:29:06 +0100

    Switch to using PostCSS for Tailwind compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto-slab": ["Roboto Slab", "serif"],
+      },
+    },
   },
-  plugins: [],
 };
